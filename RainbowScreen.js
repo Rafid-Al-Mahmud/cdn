d73a7277f8d18d388f0fa4e1c0c66b52153c77b8
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2023-04-21 21:51:16 +0600

    Update RainbowScreen.js

--- RainbowScreen.js
+++ RainbowScreen.js
@@ -7,4 +7,4 @@
   background: linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,255,0,1) 17%, rgba(50,205,50,1) 33%, rgba(0,255,255,1) 50%, rgba(0,0,255,1) 66%, rgba(255,0,255,1) 83%, rgba(255,0,0,1) 100%);
   filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#ff0000",endColorstr="#ff0000",GradientType=1);
 }
-</style>`;
+</style>`:false;
