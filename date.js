16a17df7ea91e686a7d721ff8cfe4177ee7126dd
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2023-04-28 23:18:25 +0600

    Update date.js

--- date.js
+++ date.js
@@ -7,10 +7,19 @@ $date=[
     new Date().getHours(),
     new Date().getMinutes(),
     new Date().getSeconds(),
-    new Date().getMilliseconds()
+    new Date().getMilliseconds(),
+    new Date().getTime()
 ]
 },0);
 date=(x='a')=>{
-if (typeof(x)==='number') return $date[x];
-else if (typeof(x)==='string') return $date[(x==='y')?0:(x==='m')?1:(x==='dt')?2:(x==='d')?3:(x==='h')?4:(x==='m')?5:(x==='s')?6:(x==='ms')?7:false];
+if (/[0-9]/.test(x)) return $date[x];
+else if (/y|m|dt|d|h|mi|s|ms/.test(x)) return $date[(x==='y')?0:(x==='m')?1:(x==='dt')?2:(x==='d')?3:(x==='h')?4:(x==='mi')?5:(x==='s')?6:(x==='ms')?7:(x==='t')?7:false];
+else if (x==='a') return new Date();
+else {
+    if (typeof(x)!=='number'||typeof(x)!=='string'){
+        console.warn('Use "String" as parameter of date function.')
+    }else if (/[0-9]|y|m|dt|d|h|mi|s|ms/.test(x)==='false':){
+        ?console.warn('Use [0-9]|y|m|dt|d|h|mi|s|ms'):console.error()
+    }
+}
 }
