1b8c57e0a5c68648e1ee5d45a50e36e9dfd91ec5
Rafid-Al-Mahmud <76583107+Rafid-Al-Mahmud@users.noreply.github.com>
2023-05-30 23:35:26 +0600

    Update fc.js

--- fc.js
+++ fc.js
@@ -1,4 +1,4 @@
 for (let i = 0; i < document.querySelectorAll('.fc').length; i++) {
-  const element = document.querySelectorAll('.fc-container')[i];
-  element.outerHTML="<div style='display: flex;justify-content: center;flex-wrap: wrap;flex-direction: row;align-items: center;'>"+element.outerHTML+"</div>";
+  const element = document.querySelectorAll('.fc')[i];
+  element.outerHTML="<div class='fc-container' style='display: flex;justify-content: center;flex-wrap: wrap;flex-direction: row;align-items: center;'>"+element.outerHTML+"</div>";
 }
